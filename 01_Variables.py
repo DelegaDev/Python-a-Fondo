e8cba00036ead4b2a91e02fe95442f6861fd22a0
DelegaDev <192146615+DelegaDev@users.noreply.github.com>
2024-12-22 13:58:15 +0100

    Add file via upload

---
+++ 01_Variables.py
@@ -0,0 +1,14 @@
+# 1. Declara y asigna valores a las siguientes variables:
+name, surname, city = 'Moi', 'Myself', 'Madrid'
+surname = 'Myself'
+age = '43'
+height = 1.8
+is_student = True
+nombre_completo = 'Moi Myself'
+colour = 'black'
+print (name)
+print (f'Tengo {age} años')
+print (height)
+print (is_student)
+print (len(nombre_completo))
+print (name, surname, city)

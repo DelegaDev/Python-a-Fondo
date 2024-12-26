6b8569165c1d6d1f3f64a14c64d06430b41455e5
DelegaDev <192146615+DelegaDev@users.noreply.github.com>
2024-12-26 19:28:01 +0100

    Terminado variable

--- 01_Variables.py
+++ 01_Variables.py
@@ -1,3 +1,4 @@
+'''
 # 1. Declara y asigna valores a las siguientes variables:
 name, surname, city = 'Moi', 'Myself', 'Madrid'
 surname = 'Myself'
@@ -12,3 +13,28 @@ print (height)
 print (is_student)
 print (len(nombre_completo))
 print (name, surname, city)
+
+# 6. 
+color = input ('Por favor, introduce tu color favorito: ')
+print (f'WOW! Tu color favorito es {color}')
+
+# 7. Declara una variable fruit e inicialÃ­zala con un valor. Luego, cambia el valor de la fruta a otro diferente y vuelve a imprimirla.
+fruit = 'fresa'
+print (fruit)
+fruit = 'manzana'
+print (fruit)
+
+# 8. Convierte un número decimal, almacenado en la variable price, a un número entero y luego imprímelo.
+price = 50.25
+print (int(price))
+
+# 9. Declara una variable llamada address_len y almacena en ella la cantidad de caracteres de una dirección usando la función len(). Imprime el resultado.
+address = 'Calle Mayor 123'
+address_len = len(address)
+print (address_len)
+'''
+# 10. Usa un tipo de dato forzado para declarar una variable phone, asegurándote de que siempre será un número. Luego, cambia su valor a un número diferente y verifica el tipo de la variable con type().
+phone : int = 669123456
+print (type (phone))
+phone = 99876543
+print (type(phone))

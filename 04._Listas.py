3e3c644080aaf5fb24eeb42f4edb40acef1d4629
DelegaDev <192146615+DelegaDev@users.noreply.github.com>
2024-12-30 19:08:06 +0100

    Hasta #5 Listas

---
+++ 04._Listas.py
@@ -0,0 +1,39 @@
+### Listas ###
+
+# 1. Crea una lista con los números del 1 al 5 e imprí­mela.
+
+my_list = [1,2,3,4,5]
+print (my_list)
+
+# 2. Accede e imprime el tercer elemento de la lista [10, 20, 30, 40, 50].
+
+my_list = [10,20,30,40,50]
+print (my_list [2])
+
+# 3. Agrega el número 6 al final de la lista [1, 2, 3, 4, 5] e imprí­mela.
+
+my_list = [1,2,3,4,5]
+my_list.append (6)
+print (my_list)
+
+# 4. Inserta el número 15 en la posición 2 de la lista [10, 20, 30, 40, 50].
+
+my_list = [10,20,30,40,50]
+my_list.insert (2, 15)
+print (my_list)
+
+# 5. Elimina el primer valor 30 de la lista [10, 20, 30, 30, 40, 50].
+
+my_list = [10,20,30,30,40,50]
+my_list.remove (30)
+print (my_list)
+
+# 6. Usa la función pop() para eliminar el último elemento de la lista [1, 2, 3, 4, 5] y almacénalo en una variable. Imprime la variable y la lista.
+
+# 7. Invierte la lista [100, 200, 300, 400, 500] e imprí­mela.
+
+# 8. Ordena la lista [3, 1, 4, 2, 5] en orden ascendente e imprí­mela.
+
+# 9. Concatena las listas [1, 2, 3] y [4, 5, 6] y almacena el resultado en una nueva lista. Imprime la lista resultante.
+
+# 10. Crea una sublista con los elementos de la lista [10, 20, 30, 40, 50] que van desde la posición 1 hasta la 3 (sin incluir la posición 3).

b6745a174f3a624da0681ddea9d2fc367d94ba94
DelegaDev <192146615+DelegaDev@users.noreply.github.com>
2025-01-01 22:51:05 +0100

    Finalizadas Tuplas

--- 05_Tuplas.py
+++ 05_Tuplas.py
@@ -29,10 +29,33 @@ print (tupla05.index ('Python'))
 
 # 6. Concatena dos tuplas: (1, 2, 3) y (4, 5, 6) e imprime la tupla resultante.
 
+tupla06_1 = (1, 2, 3)
+tupla06_2 = (4, 5, 6)
+sum_tuplas = tupla06_1 + tupla06_2
+print (sum_tuplas)
+print (tupla06_1 + tupla06_2)
+
 # 7. Crea una subtupla con los elementos desde la posición 2 hasta la 4 (sin incluir la 4) de la tupla (10, 20, 30, 40, 50).
+
+tupla07 = (10, 20, 30, 40, 50)
+print (tupla07 [2:4])
 
 # 8. Convierte la tupla ("rojo", "verde", "azul") en una lista, cambia el segundo elemento a "amarillo" y vuelve a convertirla en una tupla. Imprime la tupla resultante.
 
+tupla08 = ("rojo", "verde", "azul")
+tupla08 = list (tupla08)
+tupla08 [1] = 'amarillo'
+tupla08 = tuple (tupla08)
+print (tupla08)
+print (type (tupla08))
 # 9. Elimina una tupla llamada my_tuple usando del y luego intenta imprimirla para ver el resultado.
 
+my_tuple = (1, 2, 3)
+del my_tuple
+# print (my_tuple)      NameError: name 'my_tuple' is not defined
+
 # 10. Crea una tupla con un solo elemento (el número 100) e imprímela. Asegúrate de usar la sintaxis correcta para crear una tupla con un solo elemento.
+
+tupla10 = (100, )
+print (tupla10)
+print (type (tupla10))
